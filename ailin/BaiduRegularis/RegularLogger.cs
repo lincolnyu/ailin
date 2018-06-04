0540baa761b7e1f9c937aacc615dc2240e5817b6
Lincoln Yu <quanben@msn.com>
2018-06-04 11:16:56 +1000

    stub for baidu handler

---
+++ ailin/BaiduRegularis/RegularLogger.cs
@@ -0,0 +1,34 @@
+﻿using Regularis.SiteSpecific;
+using static Regularis.SiteSpecific.TiebaManager;
+
+namespace Regularis
+{
+    class RegularLogger
+    {
+        const string DefaultPresenceMesage = "到";
+
+        public TiebaManager Manager { get; } 
+
+        public void Run()
+        {
+
+        }
+
+        private void ReportVote()
+        {
+
+        }
+
+        private void ReportPresence(PostNode node, string content = DefaultPresenceMesage)
+        {
+            Manager.Post(node, content);
+        }
+
+        private void PlayIdiomGame(PostNode postNode)
+        {
+
+        }
+
+
+    }
+}
