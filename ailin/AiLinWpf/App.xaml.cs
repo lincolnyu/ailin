1b46f49dfd55f2262804e4157c47c80dafd3b294
Lincoln Yu <quanben@msn.com>
2020-04-13 20:04:42 +1000

    Bump to .net 4.8.

--- ailin/AiLinWpf/App.xaml.cs
+++ ailin/AiLinWpf/App.xaml.cs
@@ -1,9 +1,8 @@
-﻿using Squirrel;
+﻿//using Squirrel;
 using System;
 #if !NO_SINGLE_INSTANDCE
 using System.Threading;
 #endif
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace AiLinWpf
@@ -18,7 +17,6 @@ namespace AiLinWpf
     /// </remarks>
     public partial class App : Application
     {
-        const string GitHubHost = "https://github.com/lincolnyu/ailin";
 #if !NO_SINGLE_INSTANCE
         private static Mutex _mutex = new Mutex(true, "{f0791ebc-4bff-484d-8199-e945b46bbed0}");    
 #endif
@@ -27,32 +25,6 @@ namespace AiLinWpf
         App()
         {
             InitializeComponent();
-        }
-
-        private static async Task CheckForUpdate()
-        {
-            var title = _mainWindow.Title;
-            try
-            {
-                using (var mgr = await UpdateManager.GitHubUpdateManager(GitHubHost))
-                {
-                    var res = await mgr.UpdateApp();
-                    var ver = res?.Version?.Version;
-                    if (ver != null)
-                    {
-                        MessageBox.Show($"成功安装更新版本{ver.Major}.{ver.Minor}.{ver.Build}", title);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                string message = ex.Message + Environment.NewLine;
-                if (ex.InnerException != null)
-                {
-                    message += ex.InnerException.Message;
-                }
-                MessageBox.Show("无法安装更新：\n" + message, title);
-            }
         }
 
         [STAThread]
@@ -67,9 +39,6 @@ namespace AiLinWpf
                     App app = new App();
                     _mainWindow = new MainWindow();
                     app.Run(_mainWindow);
-#if !DEBUG
-                    CheckForUpdate().Wait();
-#endif
 
 #if !NO_SINGLE_INSTANCE
                 }
